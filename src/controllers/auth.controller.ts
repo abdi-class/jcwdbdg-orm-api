cc0a7f79f41c6d10064dc76037911afdf15ea46e
Abdi-01 <abdialghi@gmail.com>
2025-10-20 09:53:14 +0700

    implement validator

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,20 +4,15 @@ import prisma from "../prisma";
 class AuthController {
   public register = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      try {
-        // code
-        await prisma.accounts.create({
-          data: req.body,
-        });
+      // code
+      await prisma.accounts.create({
+        data: req.body,
+      });
 
-        res.status(200).send({
-          message: "Registration Success",
-          success: true,
-        });
-      } catch (error) {
-        console.log(error);
-        res.status(500).send(error);
-      }
+      res.status(200).send({
+        message: "Registration Success",
+        success: true,
+      });
     } catch (error) {
       console.log(error);
       res.status(500).send(error);
