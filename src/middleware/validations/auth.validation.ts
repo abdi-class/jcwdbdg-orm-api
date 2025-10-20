7f18eecdadc0809cb364c1737e265424515d4af6
Abdi-01 <abdialghi@gmail.com>
2025-10-20 10:30:42 +0700

    revamp: auth validation with validationCheck middleware

--- src/middleware/validations/auth.validation.ts
+++ src/middleware/validations/auth.validation.ts
@@ -1,6 +1,19 @@
 import { NextFunction, Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 
+const validationCheck = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw { code: 400, errors: errors.array() };
+    }
+    next(); // ini baru jalan ketika errors.isEmpty bernilai false
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const regisValidation = [
   body("username").notEmpty().withMessage("Username required"),
   body("email").notEmpty().isEmail().withMessage("Email required"),
@@ -14,16 +27,11 @@ export const regisValidation = [
       minSymbols: 0,
     })
     .withMessage("Password required"),
-  (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const errors = validationResult(req);
+  validationCheck,
+];
 
-      if (!errors.isEmpty()) {
-        throw { code: 400, errors: errors.array() };
-      }
-      next(); // ini baru jalan ketika errors.isEmpty bernilai false
-    } catch (error) {
-      next(error);
-    }
-  },
+export const loginValidation = [
+  body("email").notEmpty().isEmail().withMessage("Email required"),
+  body("password").notEmpty().withMessage("Password required"),
+  validationCheck,
 ];
