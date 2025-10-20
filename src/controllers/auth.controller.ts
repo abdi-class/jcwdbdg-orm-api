e0657645731c384fee6501b03eba80b39222d3d7
Abdi-01 <abdialghi@gmail.com>
2025-10-20 09:48:43 +0700

    implement validator

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,12 +1,26 @@
 import { NextFunction, Request, Response } from "express";
+import prisma from "../prisma";
 
 class AuthController {
   public register = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      //
+      try {
+        // code
+        await prisma.accounts.create({
+          data: req.body,
+        });
+
+        res.status(200).send({
+          message: "Registration Success",
+          success: true,
+        });
+      } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+      }
     } catch (error) {
       console.log(error);
-      res.status(200).send(error);
+      res.status(500).send(error);
     }
   };
 }
--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -1,5 +1,6 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import AuthController from "../controllers/auth.controller";
+import { body, validationResult } from "express-validator";
 
 class AuthRouter {
   private route: Router;
@@ -12,7 +13,25 @@ class AuthRouter {
   }
 
   private initializeRoutes = () => {
-    this.route.post("/regis", this.authController.register);
+    this.route.post(
+      "/regis",
+      body("email").notEmpty().isEmail().withMessage("Email required"),
+      (req: Request, res: Response, next: NextFunction) => {
+        try {
+          const errors = validationResult(req);
+
+          if (!errors.isEmpty()) {
+            // throw { code: 400, errors };
+            throw errors.array()[0].msg;
+          }
+          next(); // ini baru jalan ketika errors.isEmpty bernilai false
+        } catch (error) {
+          console.log(error);
+          res.status(500).send(error);
+        }
+      },
+      this.authController.register
+    );
   };
 
   public getRouter = (): Router => {
